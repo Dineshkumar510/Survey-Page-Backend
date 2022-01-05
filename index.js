fa18b81d8aebacddd0f34b35f511a0fcdac900aa
Dinesh Kumar <dk6089567@gmail.com>
2022-01-05 20:27:26 +0530

    changed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = process.env.port || 3000;
+const port = process.env.PORT || 8080;
 
 
 // Serve only the static files form the dist directory
